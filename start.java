aeb23d47962eab204410ff9c765e9a105d7d276e
Bruno Trova <brunotrova802@gmail.com>
2025-03-26 22:11:12 -0300

    Subindo as primeiras aulas introdutórias do curso de java

---
+++ start.java
@@ -0,0 +1,11 @@
+public class start {
+    public static void main(String[] args) {
+        int y = 72;
+        double x = 1142.4120412074;
+        System.out.println(y);
+        System.out.printf("%.2f", x);
+        System.out.println();
+        System.out.println("Olá Mundo");
+    }
+    
+}
