bd7fa813ccbb6ae5c86eac1ab61196b025e35117
Bruno Trova <brunotrova802@gmail.com>
2025-05-15 19:59:09 -0300

    aulas java

---
+++ Split.java
@@ -0,0 +1,18 @@
+public class Split {
+    public static void main(String[] args) {
+        String s = "potato apple lemon";
+        String [] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[2]);
+        System.out.println(vect[1]);
+
+
+        // Exemplo de comentario de linha
+
+         
+
+        /* Comentario em bloco   */ 
+    }
+    
+}
